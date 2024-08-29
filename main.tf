f559c1919e072e05d1af3aab4bdd1e00d9e2ecbb
olee200 <oliver_lee@comcast.com>
2024-08-29 12:07:12 -0400

    Create security group

--- main.tf
+++ main.tf
@@ -29,7 +29,8 @@ resource "aws_instance" "blog" {
 }
 
 resource "aws_security_group" "blog" {
-  name = "blog"
+  name        = "blog"
+  description = "Allow HTTP and HTTPS inbound traffic. Allow everything outbound."
   tags = {
     Terraform = "true"
   }
@@ -42,6 +43,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
   security_group_id = aws_security_group.blog.id
 }
 
@@ -52,6 +54,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
   security_group_id = aws_security_group.blog.id
 }
 
@@ -62,5 +65,6 @@ resource "aws_security_group_rule" "blog_everything_out" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
+
   security_group_id = aws_security_group.blog.id
 }
